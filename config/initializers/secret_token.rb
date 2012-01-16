0034ad21b9f18f0430941c19c46d5854c15c3c04
Felix Schymetzko <felix.schymetzko@gmail.com>
2012-01-16 22:28:50 +0100

    testing to see if heroku works

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesomedemo::Application.config.secret_token = 'd77f26614769bb044a346e79ecd71150a5269255923bae4f852c63f4f3547c1f440bcfc56a9d0e113dfa0317d99950b6a04d204acdf2cdb17af6d781f5f2e318'
